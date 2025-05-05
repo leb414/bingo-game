c17f196a1857020782f77be1afef9609be42e440
leb414 <lebpogi00@gmail.com>
2025-05-05 18:51:23 +0800

    updateURl

--- src/lib/socket.ts
+++ src/lib/socket.ts
@@ -1,5 +1,5 @@
 import { io } from 'socket.io-client';
 
-export const socket = io('http://172.20.10.5:3001', {
+export const socket = io('http://localhost:3001', {
   transports: ['websocket'],
 });
